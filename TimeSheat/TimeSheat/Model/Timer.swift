b88f1ad7497c60df2107df0d4260c28da531aada
Jonathan Law <lawjonathan17@gmail.com>
2018-03-31 00:23:48 -0700

    Start button working somewhat

--- TimeSheat/TimeSheat/Model/Timer.swift
+++ TimeSheat/TimeSheat/Model/Timer.swift
@@ -8,10 +8,94 @@
 
 import Foundation
 
-class Timer
+protocol ClockProtocol
+{
+    func timeRemainingOnTimer(_ timer: Clock, timeRemaining: TimeInterval)
+    func timerHasFinished(_ timer: Clock)
+}
+
+class Clock
 {
     var timer: Timer? = nil
     var startTime: Date?
     var duration: TimeInterval = 360      // default = 6 minutes
     var elapsedTime: TimeInterval = 0
+    var delegate: ClockProtocol?
+    
+    var isStopped: Bool {
+        return timer == nil && elapsedTime == 0
+    }
+    var isPaused: Bool {
+        return timer == nil && elapsedTime > 0
+    }
+    
+    @objc dynamic func timerAction() {
+        // 1
+        guard let startTime = startTime else {
+            return
+        }
+        
+        // 2
+        elapsedTime = -startTime.timeIntervalSinceNow
+        
+        // 3
+        let secondsRemaining = (duration - elapsedTime).rounded()
+        
+        // 4
+        if secondsRemaining <= 0 {
+            resetTimer()
+            delegate?.timerHasFinished(self)
+        } else {
+            delegate?.timeRemainingOnTimer(self, timeRemaining: secondsRemaining)
+        }
+    }
+    
+    // 1
+    func startTimer() {
+        startTime = Date()
+        elapsedTime = 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(timerAction),
+                                     userInfo: nil,
+                                     repeats: true)
+        timerAction()
+    }
+    
+    // 2
+    func resumeTimer() {
+        startTime = Date(timeIntervalSinceNow: -elapsedTime)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(timerAction),
+                                     userInfo: nil,
+                                     repeats: true)
+        timerAction()
+    }
+    
+    // 3
+    func stopTimer() {
+        // really just pauses the timer
+        timer?.invalidate()
+        timer = nil
+        
+        timerAction()
+    }
+    
+    // 4
+    func resetTimer() {
+        // stop the timer & reset back to start
+        timer?.invalidate()
+        timer = nil
+        
+        startTime = nil
+        duration = 360
+        elapsedTime = 0
+        
+        timerAction()
+    }
 }
+
+
--- TimeSheat/TimeSheat/View Controllers/ViewController.swift
+++ TimeSheat/TimeSheat/View Controllers/ViewController.swift
@@ -9,14 +9,20 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    @IBOutlet weak var timeField: NSTextField!
     
     @IBOutlet weak var helloLabel: NSTextField!
+    @IBOutlet weak var timeField: NSTextField!
     
-    @IBOutlet weak var timeLabel: NSStackView!
+    var clock = Clock()
+    
     
     @IBAction func startButtonClicked(_ sender: Any) {
+        if clock.isPaused {
+            clock.resumeTimer()
+        } else {
+            clock.duration = 360
+            clock.startTimer()
+        }
     }
     
     @IBAction func beginTimer(_ sender: Any) {
@@ -31,7 +37,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        clock.delegate = self
     }
 
     override var representedObject: Any? {
@@ -39,7 +45,39 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
+extension ViewController: ClockProtocol {
+    
+    func timeRemainingOnTimer(_ timer: Clock, timeRemaining: TimeInterval) {
+        updateDisplay(for: timeRemaining)
+    }
+    
+    func timerHasFinished(_ timer: Clock) {
+        updateDisplay(for: 0)
+    }
+}
+
+extension ViewController {
+    
+    // MARK: - Display
+    
+    func updateDisplay(for timeRemaining: TimeInterval) {
+        timeField.stringValue = textToDisplay(for: timeRemaining)
+    }
+    
+    private func textToDisplay(for timeRemaining: TimeInterval) -> String {
+        if timeRemaining == 0 {
+            return "Done!"
+        }
+        
+        let minutesRemaining = floor(timeRemaining / 60)
+        let secondsRemaining = timeRemaining - (minutesRemaining * 60)
+        
+        let secondsDisplay = String(format: "%02d", Int(secondsRemaining))
+        let timeRemainingDisplay = "\(Int(minutesRemaining)):\(secondsDisplay)"
+        
+        return timeRemainingDisplay
+    }
+    
+}
