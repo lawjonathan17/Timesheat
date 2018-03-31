5207a5c4cdb18cb04a35e2cc145537a5fe7d17a6
Jonathan Law <lawjonathan17@gmail.com>
2018-03-31 11:09:17 -0700

    Remove automatically generated comments

--- TimeSheat/TimeSheat/Model/Timer.swift
+++ TimeSheat/TimeSheat/Model/Timer.swift
@@ -1,11 +1,3 @@
-//
-//  Timer.swift
-//  TimeSheat
-//
-//  Created by Jonathan Law on 3/30/18.
-//  Copyright © 2018 Jonathan Law. All rights reserved.
-//
-
 import Foundation
 
 protocol ClockProtocol
